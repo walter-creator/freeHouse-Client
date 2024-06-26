08574b9891544dcde09da9a252ac0a8e23b10764
walter-creator <noubissiwalter@gmail.com>
2024-06-27 01:14:05 +0200

    finish with the integration of the Sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Profile from './Pages/Profile';
 import SignIn from './Pages/SignIn';
+import SignUp from "./Pages/SignUp";
 
 function App() {
 
 
   return (
     <>
-      <SignIn/>
+      <SignUp/>
     </>
   )
 }
